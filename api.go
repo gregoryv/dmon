5ed4e0dca51c6721880ef1d627ec119324031c42
Gregory Vincic <gregoryvse@gmail.com>
2023-11-09 14:36:03 +0100

    Log all requests

--- api.go
+++ api.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
 )
 
 func UpdateState(w http.ResponseWriter, r *http.Request) {
+	data, _ := httputil.DumpRequest(r, true)
+	fmt.Println(string(data))
 	fmt.Fprint(w, "hi back")
 }
